9cd15f94b7aa4e75ccf147ae3eeb888a13add491
Alison-silva <alison.ssilva@hotmail.com>
2024-09-17 14:51:03 -0300

    Primeiro commit

---
+++ src/main/java/com/alison/sistemaanuncios/SistemaanunciosApplication.java
@@ -0,0 +1,13 @@
+package com.alison.sistemaanuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaanunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaanunciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alison/sistemaanuncios/SistemaanunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alison.sistemaanuncios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaanunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
