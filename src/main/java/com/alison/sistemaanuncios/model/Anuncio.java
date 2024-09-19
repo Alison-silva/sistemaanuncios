de7d399474a0bc4b424dc982053571cdae621f35
Alison-silva <alison.ssilva@hotmail.com>
2024-09-19 11:36:46 -0300

    criação da tabela anuncio e categoria

--- src/main/java/com/alison/sistemaanuncios/model/Anuncio.java
+++ src/main/java/com/alison/sistemaanuncios/model/Anuncio.java
@@ -1,4 +1,101 @@
 package com.alison.sistemaanuncios.model;
 
-public class Anuncio {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "anuncio")
+@SequenceGenerator(name = "seq_anuncio", sequenceName = "seq_anuncio", allocationSize = 1, initialValue = 1)
+public class Anuncio implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_anuncio")
+    private Long id;
+
+    @NotEmpty(message = "O titulo não pode ser vazio!")
+    @NotNull(message = "O titulo não pode ser nulo!")
+    private String titulo;
+
+    @Lob
+    @Type(type = "org.hibernate.type.ImageType")
+    private byte[] image;
+
+    @NotEmpty(message = "Descrição não pode ser vazio!")
+    @NotNull(message = "Descrição não pode ser nulo!")
+    @Column(columnDefinition = "text")
+    private String descricao;
+
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date timestamp;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Categoria categoria;
+
+    @NotEmpty(message = "Preço não pode ser vazio!")
+    @NotNull(message = "Preço não pode ser nulo!")
+    private BigDecimal preco = BigDecimal.ZERO;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
--- src/main/java/com/alison/sistemaanuncios/model/Categoria.java
+++ src/main/java/com/alison/sistemaanuncios/model/Categoria.java
@@ -1,4 +1,36 @@
 package com.alison.sistemaanuncios.model;
 
-public class Categoria {
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "categoria")
+@SequenceGenerator(name = "seq_categoria", sequenceName = "seq_categoria", allocationSize = 1, initialValue = 1)
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria")
+    private Long id;
+
+    @NotEmpty(message = "A categoria não pode ser vazia!")
+    @NotNull(message = "A categoria não pode ser nula!")
+    private String categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
