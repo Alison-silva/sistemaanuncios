45025d01d416c25e175d1b99f595c8f8ca6f2906
Alison-silva <alison.ssilva@hotmail.com>
2024-09-27 23:48:29 -0300

    atualização de anuncio

--- src/main/java/com/alison/sistemaanuncios/controllers/AnuncioController.java
+++ src/main/java/com/alison/sistemaanuncios/controllers/AnuncioController.java
@@ -1,5 +1,6 @@
 package com.alison.sistemaanuncios.controllers;
 
+import com.alison.sistemaanuncios.model.Anuncio;
 import com.alison.sistemaanuncios.model.Usuario;
 import com.alison.sistemaanuncios.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +8,21 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Date;
 
 @Controller
 public class AnuncioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
 
     Usuario usuario = new Usuario();
 
@@ -33,6 +40,20 @@ public class AnuncioController {
 
         return model;
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "**/salvaranuncio", consumes = { "multipart/form-data" })
+    public ModelAndView salvaranuncio(@Valid Anuncio anuncio, BindingResult bindingResult, final MultipartFile file) throws Exception {
+
+        ModelAndView model = new ModelAndView("anuncio");
+
+        Date now = new Date();
+        anuncio.setTimestamp(now);
+
+
+
+        return model;
+    }
+
 
     private void buscarUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
